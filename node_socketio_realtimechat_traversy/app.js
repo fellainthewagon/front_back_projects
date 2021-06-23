765fa3e97d9b102cb3a1131d69d7cba531dbe662
fellainthewagon <fellainthewagon@gmail.com>
2021-06-24 01:52:14 +0300

    socket.io_chat chap2

--- node_socketio_realtimechat_traversy/app.js
+++ node_socketio_realtimechat_traversy/app.js
@@ -2,29 +2,69 @@ const express = require("express");
 const http = require("http");
 const path = require("path");
 const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} = require("./utils/users");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
 const PORT = process.env.PORT || 3000;
+const botName = "ChatBot";
 
 /* run when client connects */
 io.on("connection", (socket) => {
-  /* welcome current user */
-  socket.emit("message", "Welcome to ChatCord!");
+  socket.on("joinRoom", ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
 
-  /* broadcast when user connects */
-  socket.broadcast.emit("message", "A user has joined the chat");
+    socket.join(user.room);
 
-  /* when user disconnects */
-  socket.on("disconnect", () => {
-    io.emit("message", "A user has left the chat");
+    /* welcome current user */
+    socket.emit("message", formatMessage(botName, "Welcome to ChatCord!"));
+
+    /* broadcast when user connects */
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "message",
+        formatMessage(botName, `${user.username} has joined the chat`)
+      );
+
+    /* send users and room info */
+    io.to(user.room).emit("roomUsers", {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    });
   });
 
   /* listen chat message */
   socket.on("chatMessage", (msg) => {
-    io.emit("message", msg);
+    const user = getCurrentUser(socket.id);
+
+    io.to(user.room).emit("message", formatMessage(user.username, msg));
+  });
+
+  /* when user disconnects */
+  socket.on("disconnect", () => {
+    const user = userLeave(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        "message",
+        formatMessage(botName, `${user.username} has left the chat`)
+      );
+
+      /* send users and room info */
+      io.to(user.room).emit("roomUsers", {
+        room: user.room,
+        users: getRoomUsers(user.room),
+      });
+    }
   });
 });
 
--- node_socketio_realtimechat_traversy/public/js/main.js
+++ node_socketio_realtimechat_traversy/public/js/main.js
@@ -1,11 +1,29 @@
 const chatForm = document.querySelector("#chat-form");
+const chatMessages = document.querySelector(".chat-messages");
+const roomName = document.getElementById("room-name");
+const userList = document.getElementById("users");
+
+/* get info from url string */
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true,
+});
 
 const socket = io();
+
+/* join chatroom */
+socket.emit("joinRoom", { username, room });
+
+socket.on("roomUsers", ({ room, users }) => {
+  outputRoom(room);
+  outputUsers(users);
+});
 
 /* msg from server */
 socket.on("message", (message) => {
   console.log(message);
   displayMessage(message);
+
+  chatMessages.scrollTop = chatMessages.scrollHeight;
 });
 
 chatForm.addEventListener("submit", (e) => {
@@ -14,6 +32,10 @@ chatForm.addEventListener("submit", (e) => {
 
   /* emit message to server */
   socket.emit("chatMessage", msg);
+
+  /* clear input & save focus */
+  e.target.elements.msg.value = "";
+  e.target.elements.msg.focus();
 });
 
 function displayMessage(message) {
@@ -21,9 +43,19 @@ function displayMessage(message) {
 
   div.classList.add("message");
   div.innerHTML = `
-    <p class="meta">Brad <span>9:12pm</span></p>
-    <p class="text">${message}</p>
+    <p class="meta">${message.username} <span>${message.time}</span></p>
+    <p class="text">${message.text}</p>
   `;
 
   document.querySelector(".chat-messages").appendChild(div);
+}
+
+function outputRoom(room) {
+  roomName.innerHTML = room;
+}
+
+function outputUsers(users) {
+  userList.innerHTML = `
+    ${users.map((user) => `<li>${user.username}</li>`).join("")}
+  `;
 }
